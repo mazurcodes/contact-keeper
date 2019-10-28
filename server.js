bcdb9768623b7dba56c0094c19e1a6886654e6df
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-10-28 15:58:14 +0100

    Ready for heroku

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const DB_CONNECTION = require('./config').mongo;
+const path = require('path');
+const config = require("config");
+
 
 // Server
 const app = express();
 
 // Database
+const DB_CONNECTION = config.get("mongoURI");
+
 mongoose.connect(DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,6 +32,16 @@ const usersRouter = require('./routes/users');
 app.use('/api/auth', authRouter);
 app.use('/api/contacts', contactsRouter);
 app.use('/api/users', usersRouter);
+
+// Serve static assets in production
+// check if we are in production mode
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  } )
+}
 
 // Disabling some response fields
 app.disable("x-powered-by");

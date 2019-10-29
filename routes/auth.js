f8a26909c11efc5cf0038d8d039720c360355bab
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-10-29 12:46:48 +0100

    Bug fixes

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const { check, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
 const User = require("../models/User");
 
 // @route     GET api/auth

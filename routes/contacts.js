722eadd564ac23954c342f7cef68aa20749662ee
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-10-29 13:02:14 +0100

    Changed to lowercase

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
